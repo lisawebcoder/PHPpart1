e2238686a507c35fce9e1d4f43eefdc88c1f0616
Roberto <Roberto@DESKTOP-4PTR2PH>
2022-10-07 15:27:50 -0400

    created the 1st version of project 2134668 which is the mid term project i simply echo hello testing output

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+#REVISION HISTORY SECTION starts
+#DEVELOPER                DATE(yr/mm/day/                 COMMENTS
+#roberto(2134668)      2022-10-07              create a mid term project called 2134668 commit#1
+#REVISION HISTORY SECTION ends
+
+
+
+?><!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        //1st comment
+        //intial test block entry
+        echo "hello testing  output";
+        echo"<br>"
+        ?>
+    </body>
+</html>
